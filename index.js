1b09ce2b01ea38dbdf9c7083ab463dff5ad35844
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2023-04-26 18:18:19 +1000

    getting array of questions for the user and what the svg files should look like for each shape

---
+++ index.js
@@ -0,0 +1,39 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const questions = [
+    {
+        type: 'list',
+        message: 'Select a shape for your logo.',
+        name: 'shape',
+        choices: ['Circle', 'Square', 'Triangle'],
+    },
+    {
+        type: 'input',
+        name: 'shapeColour',
+        message: 'What colour would you like the shape to be? (color keyword or a hexadecimal number)'
+    },
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter text for your logo (must not be more than 3 characters).',
+    },
+    {
+        type: 'input',
+        name: 'textColour',
+        message: 'What colour would you like the text to be? (color keyword or a hexadecimal number)'
+    }
+]
+
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            console.log(answers);
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+}
+
+init();
